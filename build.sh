a94f29b45e4046cc0a25805efc0943780b229947
René Pirringer <rene@pirringer.at>
2022-09-26 18:58:33 +0200

    xcframework build works now

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+NAME="OBTableViewController"
+
+IOS_ARCHIVE_PATH="./build/archive/ios.xcarchive"
+IOS_SIMULATOR_ARCHIVE_PATH="./build/archive/ios_simulator.xcarchive"
+
+xcodebuild archive -scheme ${NAME} -archivePath ${IOS_ARCHIVE_PATH} -sdk iphoneos SKIP_INSTALL=NO #-destination 'generic/platform=iOS'
+xcodebuild archive -scheme ${NAME} -archivePath ${IOS_SIMULATOR_ARCHIVE_PATH} -sdk iphonesimulator SKIP_INSTALL=NO # -destination 'generic/platform=iOS Simulator'
+
+xcodebuild -create-xcframework \
+  -framework ${IOS_ARCHIVE_PATH}/Products/Library/Frameworks/${NAME}.framework \
+  -framework ${IOS_SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${NAME}.framework \
+  -output "./build/${NAME}.xcframework"

107d155eb666a839527775ea425f6d847b479a9e
René Pirringer <rene@pirringer.at>
2022-09-26 19:18:41 +0200

    update build and distribute

--- build.sh
+++ build.sh
@@ -5,10 +5,15 @@ NAME="OBTableViewController"
 IOS_ARCHIVE_PATH="./build/archive/ios.xcarchive"
 IOS_SIMULATOR_ARCHIVE_PATH="./build/archive/ios_simulator.xcarchive"
 
-xcodebuild archive -scheme ${NAME} -archivePath ${IOS_ARCHIVE_PATH} -sdk iphoneos SKIP_INSTALL=NO #-destination 'generic/platform=iOS'
-xcodebuild archive -scheme ${NAME} -archivePath ${IOS_SIMULATOR_ARCHIVE_PATH} -sdk iphonesimulator SKIP_INSTALL=NO # -destination 'generic/platform=iOS Simulator'
+echo "Build"
+xcodebuild archive -scheme ${NAME} -archivePath ${IOS_ARCHIVE_PATH} -sdk iphoneos SKIP_INSTALL=NO -destination 'generic/platform=iOS'
+xcodebuild archive -scheme ${NAME} -archivePath ${IOS_SIMULATOR_ARCHIVE_PATH} -sdk iphonesimulator SKIP_INSTALL=NO -destination 'generic/platform=iOS Simulator'
 
+
+echo "Create xcframework"
 xcodebuild -create-xcframework \
   -framework ${IOS_ARCHIVE_PATH}/Products/Library/Frameworks/${NAME}.framework \
   -framework ${IOS_SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${NAME}.framework \
   -output "./build/${NAME}.xcframework"
+
+
